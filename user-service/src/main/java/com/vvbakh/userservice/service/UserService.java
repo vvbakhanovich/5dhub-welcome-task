30e0ceca84fd1096f9c7eb49d3539787042341f8
vvbakhanovich <vv.bakh@yandex.ru>
2024-09-08 17:12:18 +0300

    feat: Implement find user by lastname

--- user-service/src/main/java/com/vvbakh/userservice/service/UserService.java
+++ user-service/src/main/java/com/vvbakh/userservice/service/UserService.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface UserService {
     List<User> getUsersByIdsIn(List<Long> id);
+
+    User findUserByLastname(String surname);
 }
--- user-service/src/main/java/com/vvbakh/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/vvbakh/userservice/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.vvbakh.userservice.service;
 
+import com.vvbakh.userservice.exception.NotFoundException;
 import com.vvbakh.userservice.model.User;
 import com.vvbakh.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,5 +21,13 @@ public class UserServiceImpl implements UserService {
         final List<User> users = userRepository.findUsersByIdIn(id);
         log.info("Found users by ids, number of users '{}'.", users.size());
         return users;
+    }
+
+    @Override
+    public User findUserByLastname(String lastname) {
+        final User user = userRepository.findUserByLastname(lastname)
+                .orElseThrow(() -> new NotFoundException("User not found with lastname :: " + lastname));
+        log.info("Found user with lastname '{}'.", lastname);
+        return user;
     }
 }
