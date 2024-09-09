77fa0ba57053f0dede834f86e7439a9b05b6d276
vvbakhanovich <vv.bakh@yandex.ru>
2024-09-09 07:43:35 +0300

    feat: Add exception handlers

---
+++ company-service/src/main/java/com/vvbakh/companyservice/exception/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.vvbakh.companyservice.exception;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+
+public record ErrorResponse(Map<String, String> errors,
+                            Integer status,
+                            LocalDateTime timestamp) {
+}
+
